343e44916d88be3ee0db2fe24e48fd03475cc168
Rodolfo Baume L <74028526+RodolfoBaume@users.noreply.github.com>
2022-03-04 11:51:28 -0600

    Add files via upload

---
+++ 02 - HTML/02-Codigo Pasteleria/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <title>Pastelería - La Cósmica</title>
+</head>
+<body>
+
+  	<!-- Header -->
+<header class="header-main header-position container-fluid no-padding">
+    <!-- Menu Block -->
+    <div class="menu-block container-fluid no-padding">
+        <div class="container">
+            <!-- Navigation -->
+            <nav class="navbar ow-navigation">
+                <div class="navbar-header">						
+                    <a class="navbar-brand" href="index.html" title="Logo"> <img src="img/logo.png" alt="logo" width="150px"> </a>
+                </div>
+
+
+                <div class="navbar-collapse collapse navbar-right" id="navbar">
+                    <ul class="nav navbar-nav menubar">
+                        <li class="dropdown active">
+                            <a href="index.html">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="pages/contacto.html">Contacto</a>
+                        </li>
+						<li>
+                            <a href="pages/orden.html">Ordena en línea</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav><!-- Navigation /- -->
+        </div><!-- Menu Block /- -->
+    </div>
+</header><!-- Header /- -->
+  
+
+
+    <!-- Page Banner -->
+    <div class="page-banner container-fluid no-padding">
+        <div class="page-banner-content">
+            <h3>Bienvenidos a Pastelería <span>La Cósmica</span></h3>
+        </div>
+    </div><!-- Page Banner /- -->
+  
+	<!-- Menu Sabores -->
+		<div id="menu-section" class="menu-section container-fluid no-padding">
+			
+			<div class="container">
+				<div class="section-header">
+					<h3>Nuestro Menu</h3>
+					<h5>Sabores</h5>
+					<img src="./img/section-seprator.png" alt="section-seprator" width="169" height="15">
+					<p>Los mejores pasteles de la ciudad. Pasteles hermosos y deliciosos. Servicio a domicilio. Gelatinas, cupcakes, galletas, brownies, pays
+                        y panes súper caseros. ¡Ven a probarlos!</p>
+				</div><!-- Section Header /-  -->
+			</div><!-- Container /- -->
+			
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<div class="menu-box">
+					<img src="./img/sabores/pastel_cajeta.jpg" alt="Cajeta" width="300">
+					<h3>Pastel de Cajeta <span>$230.00</span></h3>
+					<p>Relleno de Cajeta con Queso Philadelphia y nuez y un delicioso adorno de chocolate blanco. Imperdible.</p>
+				</div>
+			</div>
+				
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<div class="menu-box">
+					<img src="./img/sabores/fresas.jpg" alt="Fresas con Crema" width="300">
+					<h3>Fresas con Crema <span>$220.00</span></h3>
+					<p>Pastel de tres leches relleno de fresas naturales, crema batida y nuestra mermelada especial.</p>
+				</div>
+			</div>
+				
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<div class="menu-box">
+					<img src="./img/sabores/chocoqueso.jpg" alt="menu" width="300">
+					<h3>Chocoqueso <span>$220.00</span></h3>
+					<p>Delicioso pan de queso con una rebanada de pan de chocolate y una de tres leches con cobertura de chocolate semi amargo.</p>
+				</div>
+			</div>
+				
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<div class="menu-box">
+					<img src="./img/sabores/pinnon.jpg" alt="menu" width="300">
+					<h3>Pastel de Piñon <span>$260.00</span></h3>
+					<p>Es la combinación perfecta. Piñones, chocolate blanco y queso Philadelphia. Simplemente inigualable.</p>
+				</div>
+			</div>
+				
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<div class="menu-box">
+					<img src="./img/sabores/frutos_rojos.jpg" alt="menu" width="300">
+					<h3>Pastel de Frutos Rojos <span>$220.00</span></h3>
+					<p>Pastel de 3 leches con relleno de fresa, cereza y zarzamora cubierto con crema batida y un adorno de fruta.</p>
+				</div>
+			</div>
+				
+			<div class="col-md-6 col-sm-6 col-xs-6">
+				<div class="menu-box">
+					<img src="./img/sabores/mousse_choco.jpg" alt="menu" width="300">
+					<h3>Mousse de Chocolate<span>$220.00</span></h3>
+					<p>Pan de chocolate con 3 leches con 2 capas de mousse de chocolate, chispas de chocolate y cobertura de chocolate blanco.</p>
+				</div>
+			</div>
+		</div><!-- Menu Sabores /- -->
+
+
+        <!-- Section Adornos-->
+		<div class="festival-section container-fluid no-padding">
+			<div class="container">
+				<div class="festival-details-tab">
+                    <h3>Adornos</h3>
+					
+					<!-- Nav tabs -->
+					<ul class="nav nav-tabs" role="tablist">
+						<li class="active"><a href="" ><i><img src="./img/adornos/chispas.png" alt="Chispas" width="37" height="42"></i>Chispas</a></li>
+						<li class="active"><a href="" ><i><img src="./img/adornos/chocoBco.png" alt="Chocolate Blanco" width="37" height="42"></i>Chocolate <br> Blanco</a></li>
+						<li class="active"><a href="" ><i><img src="./img/adornos/fondant.png" alt="Fondant" width="37" height="42"></i>Fondant</a></li>
+						<li class="active"><a href="" ><i><img src="./img/adornos/cerezas.png" alt="Cerezas" width="37" height="42"></i>Cerezas</a></li>
+						<li class="active"><a href="" ><i><img src="./img/adornos/crema.png" alt="Crema Batida" width="37" height="42"></i>Crema <br> Batida</a></li>
+						
+					</ul>
+				</div>
+			</div><!-- Container /- -->
+		</div><!-- Section Adornos/- -->
+		
+  		
+        <!-- Order Section -->
+          <div class="order-section container-fluid no-padding">
+			<div class="container">
+				<div class="row">
+					<a href="pages/orden.html" title="Order Now" class="order-now">Ordenar en Línea</a>
+				</div>
+			</div><!-- Container /- -->
+		</div><!-- Order Section /- -->
+ 
+</body>
+</html>
